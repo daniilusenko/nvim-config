59ecd261c855b23aa21acce530ee93f2cdb6b3ac
daniilusenko <daniil.usenko@protonmail.com>
2023-05-08 20:00:52 +0200

    commented out linux-only config

--- lua/shared/set.lua
+++ lua/shared/set.lua
@@ -4,7 +4,6 @@ if vim.loop.os_uname().sysname == "Windows" then
 	vim.api.nvim_exec('language en_US', true)
 end
 
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -18,7 +17,10 @@ vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+-- only works on linux? need correct handling 
+-- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
