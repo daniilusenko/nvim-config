0b7fe13a99c5a01f4768a91f8ee3b06a217fa94c
daniilusenko <daniil.usenko@protonmail.com>
2023-05-23 15:30:17 +0200

    highlighting search results

--- lua/shared/set.lua
+++ lua/shared/set.lua
@@ -25,7 +25,7 @@ vim.opt.backup = false
 
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true -- hightlight search results
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
